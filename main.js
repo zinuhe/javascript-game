c27f9362dca737748600459bf3639711921bb39d
JimmyS <11039182+zinuhe@users.noreply.github.com>
2024-02-12 00:36:38 -0500

    Small changes

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const context = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 400;
 
-// Draw something
+// Draw a background
 context.fillStyle = '#909090' // Background
 context.fillRect(0, 0, canvas.width, canvas.height)
+
+// Draw a rectangles
+context.fillStyle = 'yellow'
+context.fillRect(0, 0, 20, 20) // Rectangle on upper left corner
+context.fillRect(canvas.width - 20, 0, 20, 20) // Rectangle on upper right corner
