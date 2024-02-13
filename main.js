3973e91f93c527687d79afbd06b4a88b9685b95f
JimmyS <11039182+zinuhe@users.noreply.github.com>
2024-02-12 23:03:17 -0500

    Add main

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // 1 - Initialize canvas
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
 
-// Board dimentions
-canvas.width = 800;
-canvas.height = 400;
+// Canvas dimentions
+canvas.width = 800
+canvas.height = 400
 
 // Draw a background
-context.fillStyle = '#909090' // Background
+context.fillStyle = '#909090' // Grey Background
 context.fillRect(0, 0, canvas.width, canvas.height)
 
-// Draw a rectangles
+// Draw rectangles
 context.fillStyle = 'yellow'
 context.fillRect(0, 0, 20, 20) // Rectangle on upper left corner
 context.fillRect(canvas.width - 20, 0, 20, 20) // Rectangle on upper right corner
